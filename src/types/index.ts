68b8b3c62e191bb0739e0d6b2cc9c2533cfdc9a2
szy0syz <szy0syz@gmail.com>
2019-11-17 22:12:06 +0800

    feat(index.ts xhr.ts types/index.ts): 完成项目最小系统功能

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
