138b53040880eac9a66f71a35390ceb203276557
szy0syz <szy0syz@gmail.com>
2019-11-17 23:14:33 +0800

    feat(utils/data.ts): 完成post方法遇到普通Object的data数据转换

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildURL } from './utils/url'
+import { transformRequest } from './utils/data'
 
 function axios(config: AxiosRequestConfig) {
+  processConfig(config)
   xhr(config)
+}
+
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+  config.data = transfromRequestData(config)
+}
+
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
+}
+
+function transfromRequestData(config: AxiosRequestConfig): any {
+  return transformRequest(config.data)
 }
 
 export default axios
--- src/utils/util.ts
+++ src/utils/util.ts
@@ -3,10 +3,14 @@ import { type } from 'os'
 // 缓存 / 懒加载
 const toString = Object.prototype.toString
 
-export function isDate(val: any): boolean {
+export function isDate(val: any): val is Date {
   return toString.call(val) === '[object Date]'
 }
 
-export function isObject(val: any): boolean {
-  return val !== null && typeof val === 'object'
+// export function isObject(val: any): boolean {
+//   return val !== null && typeof val === 'object'
+// }
+
+export function isPlainObject(val: any): val is Object {
+  return toString.call(val) === '[object Object]'
 }
