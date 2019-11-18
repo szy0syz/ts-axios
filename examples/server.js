52093ab692b85238d0e0050ad3c54e13ac3d8ecf
szy0syz <szy0syz@gmail.com>
2019-11-18 09:45:24 +0800

    test(examples/base/*): 新增post相关测试案例

--- examples/server.js
+++ examples/server.js
@@ -9,13 +9,15 @@ const app = express()
 const compiler = webpack(WebpackConfig)
 const router = express.Router()
 
-app.use(webpackDevMiddleware(compiler, {
-  publicPath: '/__build__/',
-  stats: {
-    colors: true,
-    chunks: false
-  }
-}))
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: '/__build__/',
+    stats: {
+      colors: true,
+      chunks: false
+    }
+  })
+)
 
 app.use(webpackHotMiddleware(compiler))
 
@@ -27,6 +29,27 @@ app.use(bodyParser.urlencoded({ extended: true }))
 router.get('/simple/get', function(req, res) {
   res.json({
     msg: `hello world`
+  })
+})
+
+router.get('/base/get', function(req, res) {
+  res.json(req.query)
+})
+
+router.post('/base/post', function(req, res) {
+  res.json(req.body)
+})
+
+router.post('/base/buffer', function(req, res) {
+  let msg = []
+  req.on('data', chunk => {
+    if (chunk) {
+      msg.push(chunk)
+    }
+  })
+  req.on('end', () => {
+    let buf = Buffer.concat(msg)
+    res.json(buf.toJSON())
   })
 })
 
