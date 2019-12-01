4bb175b452d427d224531f13716f6516ded9f53a
szy0syz <szy0syz@gmail.com>
2019-12-01 23:08:27 +0800

    Update 10章example demo

--- examples/cancel/app.ts
+++ examples/cancel/app.ts
@@ -3,36 +3,44 @@ import axios, { Canceler } from '../../src/index'
 const CancelToken = axios.CancelToken
 const source = CancelToken.source()
 
-axios.get('/cancel/get', {
-  cancelToken: source.token
-}).catch(function(e) {
-  if (axios.isCancel(e)) {
-    console.log('Request canceled', e.message)
-  }
-})
+//* "/cancel/get" 和 "/cancel/post" 服务端都是延迟 1000ms 做响应
+
+axios
+  .get('/cancel/get', {
+    cancelToken: source.token
+  })
+  .catch(function(e) {
+    if (axios.isCancel(e)) {
+      console.log('Request canceled', e.message)
+    }
+  })
 
 setTimeout(() => {
   source.cancel('Operation canceled by the user.')
 
-  axios.post('/cancel/post', { a: 1 }, { cancelToken: source.token }).catch(function(e) {
-    if (axios.isCancel(e)) {
-      console.log(e.message)
-    }
-  })
+  setTimeout(() => {
+    axios.post('/cancel/post', { a: 1 }, { cancelToken: source.token }).catch(function(e) {
+      if (axios.isCancel(e)) {
+        console.log(e.message)
+      }
+    })
+  }, 100)
 }, 100)
 
 let cancel: Canceler
 
-axios.get('/cancel/get', {
-  cancelToken: new CancelToken(c => {
-    cancel = c!
+axios
+  .get('/cancel/get', {
+    cancelToken: new CancelToken(c => {
+      cancel = c!
+    })
   })
-}).catch(function(e) {
-  if (axios.isCancel(e)) {
-    console.log('Request canceled')
-  }
-})
+  .catch(function(e) {
+    if (axios.isCancel(e)) {
+      console.log('Request canceled')
+    }
+  })
 
 setTimeout(() => {
   cancel()
-}, 200)
+}, 500)
